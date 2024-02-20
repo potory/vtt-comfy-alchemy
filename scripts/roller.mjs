642d153e721a721c5d7bc3bb66bc49d28ce84bc0
Alexey Potory <alexey.potory@gmail.com>
2024-02-20 10:04:54 +0500

    Fix

--- scripts/roller.mjs
+++ scripts/roller.mjs
@@ -1,3 +1,4 @@
+const MODULE_ID = "Comfy Alchemy";
 Hooks.once('setup', async function comfyAlchemySetup() {
     console.log(MODULE_ID, '|', `Initializing ${MODULE_ID}`);
     console.log(CONFIG.DND5E);
