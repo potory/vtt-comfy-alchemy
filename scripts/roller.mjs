4ae090ee8c70a6b481330fcd9fa96901871cc21b
Alexey Potory <alexey.potory@gmail.com>
2024-02-20 10:11:02 +0500

    Experiment

--- scripts/roller.mjs
+++ scripts/roller.mjs
@@ -1,11 +1,15 @@
 const MODULE_ID = "Comfy Alchemy";
 Hooks.once('setup', async function comfyAlchemySetup() {
-    console.log(MODULE_ID, '|', `Initializing ${MODULE_ID}`);
-    console.log(CONFIG.DND5E);
+    print(`Initializing ${MODULE_ID}`);
+    print(CONFIG.DND5E.lootTypes);
 
     CONFIG.DND5E.lootTypes['ingridient'] = {
         label: "Ингридиент"
     };
 
-    console.log(CONFIG.DND5E);
+    print(CONFIG.DND5E.lootTypes);
 });
+
+function print(message) {
+    console.log(MODULE_ID, '|', message);
+}
