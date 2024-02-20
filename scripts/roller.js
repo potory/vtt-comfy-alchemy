b3b16cbf140afdef0ca505a6abb677ad25198dbb
Alexey Potory <alexey.potory@gmail.com>
2024-02-20 09:46:23 +0500

    Console log

--- scripts/roller.js
+++ scripts/roller.js
@@ -1,6 +1,7 @@
 console.log("Hello World! This code runs immediately when the file is loaded.");
 
 Hooks.on("init", function() {
+    console.log(CONFIG.DND5E.lootTypes);
     CONFIG.DND5E.lootTypes.alchemy = {
         label: "Алхимический ингридиент"
     };
